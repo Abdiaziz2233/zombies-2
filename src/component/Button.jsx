a6535d9f766042a4f6655044408efaf90ea3e7c2
Abdiaziz2233 <zizoalhusyny73@gmail.com>
2024-04-17 12:26:32 +0300

    completed

---
+++ src/component/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+function Button(){
+  const handleClick =(e)=>e.target.textContent ="OUCH!";
+  return (<button onDoubleClick={(e) => handleClick(e)}>click me</button>)
+}
+export default Button
